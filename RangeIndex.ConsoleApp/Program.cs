12124f8a1724ff8dafad0267f1a492fbeb30f3af
Salih Cantekin <salihcantekin@gmail.com>
2022-06-30 18:39:36 +0100

    Add project files.

---
+++ RangeIndex.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+var numbers = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+Range r = new Range(2, 5);
+r = Range.StartAt(6);
+r = Range.EndAt(4);
+
+
+
+
+//Index index = Index.FromEnd(1);
+//index = new Index(numbers.Length - 1);
+
+//var first = numbers[^2];
+
+var rangeArr = numbers[r];
+
+
+rangeArr = numbers[2..3];
+
+
+foreach (var arrItem in rangeArr)
+{
+    Console.WriteLine(arrItem);
+}
+
+Console.ReadLine();
+
+
+
